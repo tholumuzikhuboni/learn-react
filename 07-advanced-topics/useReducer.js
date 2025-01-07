31d04c8ab87043540f5178e781d963812c2b22d8
Tholumuzi Khuboni <contact@tholumuzi.co.za>
2025-01-07 15:28:23 +0200

    Create useReducer.js

---
+++ 07-advanced-topics/useReducer.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from 'react';
+
+// Initial State
+const initialState = { count: 0 };
+
+// Reducer Function
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1 };
+        case 'reset':
+            return initialState;
+        default:
+            throw new Error('Invalid action type');
+    }
+};
+
+const CounterWithReducer = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <div>
+            <p>Count: {state.count}</p>
+            <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+            <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+        </div>
+    );
+};
+
+export default CounterWithReducer;
